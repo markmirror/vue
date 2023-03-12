70183fe597eba088c708956f115b2732cecaf8f6
Hsiaoming Yang <me@lepture.com>
2023-03-12 22:59:08 +0900

    fix: update Editor interface

--- src/Editor.ts
+++ src/Editor.ts
@@ -1,10 +1,11 @@
 import { markRaw } from "vue"
-import { MarkMirror, MarkMirrorOptions } from "@markmirror/core"
+import { Extension } from "@codemirror/state"
+import { MarkMirror } from "@markmirror/core"
 
 
 export class Editor extends MarkMirror {
-  constructor (options: MarkMirrorOptions = {}) {
-    super(options)
+  constructor (extensions: Extension[]) {
+    super(extensions)
     // MUST use `markRaw` to avoid Vue's proxy behavior
     markRaw(this)
   }
